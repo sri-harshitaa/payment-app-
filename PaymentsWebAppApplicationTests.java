01262ee73aef65a3fbe8f05bf4f11e9ea9dae60e
Tadimeti Venkata Sri harshita <harshitatadimeti2002@gmail.com>
2025-04-06 22:59:38 +0530

    Add files via upload

---
+++ PaymentsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaymentsWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
